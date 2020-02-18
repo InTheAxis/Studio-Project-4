f71f3a8991fe63ad0b94b5388243b89a247057b8
K0019 <39440047+K0019@users.noreply.github.com>
2020-02-18 16:04:59 +0800

    Merge with Terence
    Updated camera, prefab layers

--- Assets/Scripts/Gameplay/Character/CharTPCamera.cs
+++ Assets/Scripts/Gameplay/Character/CharTPCamera.cs
@@ -4,16 +4,85 @@ using UnityEngine;
 
 public class CharTPCamera : MonoBehaviour
 {
-    public CharTPController target;
-    
-    [SerializeField]
-    private float targetCamDist;
+    public CharTPController target;    
+    public float targetCamDist;
+    public float moveCloserDuration;
+    public float initialCamDist;
+    public LayerMask mask;
+    public float camAdjustSpeed = 10;
+    public float adjustOffset = 1;
 
+    private float curCamDist;
+    private float timer;
+    private float moveCloserDist;
+    private Camera cam;
+    private Vector3[] clipPoint;
+    private Vector3 desiredPos;
+
+    private void Start()
+    {
+        curCamDist = initialCamDist;
+        moveCloserDist = targetCamDist;
+        timer = 0;
+
+        cam = GetComponent<Camera>();
+
+        clipPoint = new Vector3[5];
+        UpdateCameraClipPoints();
+    }   
     private void FixedUpdate()
     {
+        //calculate if shld move closer
+        CalculateObstruction();
+        curCamDist = Mathf.Lerp(curCamDist, moveCloserDist, Time.deltaTime * camAdjustSpeed);
+
+        if (moveCloserDist < 2)
+            Debug.Log("player shld become transluscent");
+
         //move cam
-        transform.position = target.transform.position - (target.lookDir * targetCamDist);
+        desiredPos = target.transform.position - (target.lookDir * targetCamDist);
+        transform.position = target.transform.position - (target.lookDir * curCamDist);
         //rotate cam
         transform.LookAt(target.transform);
+    }
+
+    private void UpdateCameraClipPoints()
+    {
+        float z = cam.nearClipPlane;
+        float x = Mathf.Tan(Mathf.Deg2Rad * cam.fieldOfView * 0.5f) * z;
+        float y = x / cam.aspect;
+        Quaternion rot = Quaternion.LookRotation(transform.forward, transform.up);
+
+        clipPoint[0] = rot * new Vector3(x, y, z) + desiredPos;
+        clipPoint[1] = rot * new Vector3(-x, y, z) + desiredPos;
+        clipPoint[2] = rot * new Vector3(x, -y, z) + desiredPos;
+        clipPoint[3] = rot * new Vector3(-x, -y, z) + desiredPos;
+        clipPoint[4] = desiredPos - transform.forward;
+
+        Debug.DrawLine(clipPoint[0], target.transform.position, Color.white);
+        Debug.DrawLine(clipPoint[1], target.transform.position, Color.white);
+        Debug.DrawLine(clipPoint[2], target.transform.position, Color.white);
+        Debug.DrawLine(clipPoint[3], target.transform.position, Color.white);
+        Debug.DrawLine(clipPoint[4], target.transform.position, Color.green);
+    }
+
+    private void CalculateObstruction()
+    {
+        UpdateCameraClipPoints();
+        float minDist = targetCamDist + adjustOffset;
+        Vector3 dir;
+        float dist;
+        for (int i = 0; i < 5; ++i)
+        {
+            dir = clipPoint[i] - target.transform.position;
+            if (Physics.Raycast(target.transform.position, dir.normalized, out RaycastHit hitInfo, dir.magnitude, mask))
+            {
+                dist = hitInfo.distance;
+                if (dist < minDist)
+                    minDist = dist;
+            }
+            
+        }
+        moveCloserDist = Mathf.Clamp(minDist - adjustOffset, 0.1f, targetCamDist);
     }
 }
