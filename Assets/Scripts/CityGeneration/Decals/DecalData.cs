87718fad8730f3ee7a4ac9713160f7c67563bafd
Chindianese-HomePC <42330555+Chindianese@users.noreply.github.com>
2020-02-20 17:01:58 +0800

    spawn flowers as entities

---
+++ Assets/Scripts/CityGeneration/Decals/DecalData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+[GenerateAuthoringComponent]
+public struct DecalData : IComponentData
+{
+    // Add fields to your component here. Remember that:
+    //
+    // * A component itself is for storing data and doesn't 'do' anything.
+    //
+    // * To act on the data, you will need a System.
+    //
+    // * Data in a component must be blittable, which means a component can
+    //   only contain fields which are primitive types or other blittable
+    //   structs; they cannot contain references to classes.
+    //
+    // * You should focus on the data structure that makes the most sense
+    //   for runtime use here. Authoring Components will be used for
+    //   authoring the data in the Editor.
+    public int numRock;
+
+    public Entity rockA;
+    public Entity rockB;
+    public Entity rockC;
+    public Entity rockD;
+    public Entity rockE;
+    public Entity rockF;
+    public Entity rockG;
+    //public Entity rockC;
+}
