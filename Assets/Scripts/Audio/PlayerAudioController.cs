4e546e73637cd23cd2ddd2c7f34ad892f6002185
Chindianese-HomePC <42330555+Chindianese@users.noreply.github.com>
2020-02-24 15:02:58 +0800

    add default non terrain audio

--- Assets/Scripts/Audio/PlayerAudioController.cs
+++ Assets/Scripts/Audio/PlayerAudioController.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class PlayerAudioController : AudioController
 {
+    [SerializeField]
+    private LayerMask layerMask;
+
     public void Step()
     {
-        TerrainManager.TerrainType type = TerrainManager.instance.GetActiveTerrainTextureIdx(transform.position);
-        switch (type)
+        // check terrain or misc
+        RaycastHit hit;
+        if (!Physics.Raycast(transform.position, -Vector3.up, out hit, 2, layerMask))
+            return;
+
+        if (hit.transform.tag == "Terrain")
         {
-            case TerrainManager.TerrainType.STONE:
-                Play("footstep_stone0");
-                break;
+            TerrainManager.TerrainType type = TerrainManager.instance.GetActiveTerrainTextureIdx(transform.position);
+            switch (type)
+            {
+                case TerrainManager.TerrainType.STONE:
+                    Play("footstep_stone0");
+                    break;
 
-            case TerrainManager.TerrainType.DIRT:
-                Play("footstep_dirt0");
-                break;
+                case TerrainManager.TerrainType.DIRT:
+                    Play("footstep_dirt0");
+                    break;
 
-            default:
-                Debug.LogError("Terrain footstep audio not set.");
-                break;
+                default:
+                    Debug.LogError("Terrain footstep audio not set.");
+                    break;
+            }
         }
+        else
+            Play("footstep_stone0");
     }
 
     public void PickUpDebris()
