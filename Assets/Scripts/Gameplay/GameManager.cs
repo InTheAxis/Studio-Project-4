f99fb6daa10c3f81d730725d5ed1597528b627ac
InTheAxis <codeaxex@gmail.com>
2020-02-25 11:20:36 +0800

    edited gamemanager to look for camera on start instead of awake

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -24,7 +24,11 @@ public class GameManager : MonoBehaviour
             isHuman = !PhotonNetwork.IsMasterClient;
 
         playerObj = PhotonNetwork.Instantiate(isHuman ? humanPrefab.name : monsterPrefab.name, new Vector3(0.0f, 4.0f, 0.0f), Quaternion.identity);
-        if (CharTPCamera.Instance != null)
+    }
+
+    private void Start()
+    {
+                if (CharTPCamera.Instance != null)
             CharTPCamera.Instance.SetCharController(playerObj.GetComponent<CharTPController>());
     }
 }
